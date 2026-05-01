4ca10d663eff80971a5c52f2e3c0a0dbe46e3098
propagatorgr <propagatorgr@users.noreply.github.com>
2026-05-01 22:02:24 +0300

    Create projects.js

---
+++ projects.js
@@ -0,0 +1,34 @@
+const projects = [
+  {
+    title: "Στάσιμο κύμα σε χορδή",
+    repo: "stasima"
+  },
+  {
+    title: "Στάσιμα κύματα – εναλλακτική προσέγγιση",
+    repo: "stasima2"
+  },
+  {
+    title: "Ταλάντωση ελατηρίου",
+    repo: "spring"
+  },
+  {
+    title: "Βολή στο επίπεδο",
+    repo: "voli"
+  },
+  {
+    title: "Ανάλογα ποσά",
+    repo: "analoga-possa"
+  },
+  {
+    title: "Περιοδικός Πίνακας",
+    repo: "periodicTable"
+  },
+  {
+    title: "Μοντέλο καρδιάς",
+    repo: "heart"
+  },
+  {
+    title: "Third simulation",
+    repo: "third_sim"
+  }
+];
